c4dc94808b477f63488e851e26ff4c9673ec1a3e
Dernival Liandro <liandro.sys@gmail.com>
2023-07-30 22:19:36 -0300

    Script login successfully

--- LoginExampleUITests/LoginExampleUITests.swift
+++ LoginExampleUITests/LoginExampleUITests.swift
@@ -11,24 +11,30 @@ import XCTest
 final class LoginExampleUITests: XCTestCase {
 
     override func setUpWithError() throws {
-        // Put setup code here. This method is called before the invocation of each test method in the class.
-
-        // In UI tests it is usually best to stop immediately when a failure occurs.
         continueAfterFailure = false
-
-        // In UI tests it’s important to set the initial state - such as interface orientation - required for your tests before they run. The setUp method is a good place to do this.
     }
 
     override func tearDownWithError() throws {
-        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        
     }
 
-    func testExample() throws {
-        // UI tests must launch the application that they test.
+    func testLogin() throws {
         let app = XCUIApplication()
         app.launch()
-
-        // Use XCTAssert and related functions to verify your tests produce the correct results.
+        
+        let textHome = app.staticTexts["Bem Vindo"]
+        XCTAssert(textHome.exists)
+        
+        let fieldEmailAddress = app.textFields["login_username"]
+        fieldEmailAddress.tap()
+        fieldEmailAddress.typeText("liandro.sys@gmail.com")
+        
+        let fieldPassword = app.textFields["login_password"]
+        fieldPassword.tap()
+        fieldPassword.typeText("1234567")
+        
+        let btnContinue = app.buttons["login_button"]
+        btnContinue.tap()
     }
 
 }
